40354ef0cd4d051edaf891535b94c8cc7d17e16b
Roswer Garcia <roswer.garcia@schindler.com>
2019-05-02 13:30:17 +0200

    fixes in style

--- ArtNews.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ ArtNews.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "574f6152-15b8-4b4c-a44b-ad408d01b9f5",
+		"XAMARIN_BUILD_ID", "e19c6e09-c72f-439f-8954-2d09d375fb1c",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
